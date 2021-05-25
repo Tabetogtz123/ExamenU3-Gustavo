ac64fc4ed24d40a482dfef2b3cbb9a28a31ee4b4
Tabetogtz123 <tabetogtz20@gmail.com>
2021-05-24 23:18:40 -0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './Components/about/about.component';
+import { BuscadorComponent } from './Components/buscador/buscador.component';
+import { HeroeComponent } from './Components/heroe/heroe.component';
+import { HeroeTarjetaComponent } from './Components/heroe-tarjeta/heroe-tarjeta.component';
+import { HeroesComponent } from './Components/heroes/heroes.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BuscadorComponent,
+    HeroeComponent,
+    HeroeTarjetaComponent,
+    HeroesComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
